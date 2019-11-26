c712ed574b50013e592e65ebcd04f652958cd0a0
BWaldenmaier <BWaldenmaier@gmx.net>
2019-11-26 16:21:16 +0100

    request server datei

--- server.js
+++ server.js
@@ -5,6 +5,13 @@ var io = require('socket.io').listen(server);
 var SocketIOFile = require('socket.io-file');
 var ss = require('socket.io-stream');
 
+const request = require('request');
+request('https://eu-de.functions.cloud.ibm.com/api/v1/web/Alexander.Bartuli%40Student.Reutlingen-University.DE_dev/hrt-demo/identify-and-translate', function (error, response, body) {
+  console.error('error:', error); // Print the error if one occurred
+  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  console.log('body:', body); // Print the HTML for the Google homepage.
+});
+
 
 users = [];
 connections = [];
