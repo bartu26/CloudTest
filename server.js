ce3d6f7342fdd88757aa78f37e0f0d2aa23ffaef
bartu26 <bartu26@gmx.de>
2019-12-17 13:43:54 +0100

    port 8443

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ var options = {
 
 var app = express();
 
-server = https.createServer(options, app).listen(process.env.PORT || 3000);
+server = https.createServer(options, app).listen(process.env.PORT || 8443);
 
 ////initilization for http redirection
 //var http = require('http');
