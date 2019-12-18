b6db38539b72733a582625c7dadc29796bea9619
BWaldenmaier <BWaldenmaier@gmx.net>
2019-12-18 15:52:53 +0100

    directives?

--- server.js
+++ server.js
@@ -92,8 +92,16 @@ app.get('/socket.io-file-client.js', (req, res, next) => {
 
 app.use(csp({
     directives: {
-        defaultSrc: [`'self'`],
-        imgSrc: [`'self'`, `imgur.com`]
+        defaultSrc: ["'self'", 'default.com'],
+        scriptSrc: ["'self'", "'unsafe-inline'"],
+        styleSrc: ['style.com'],
+        fontSrc: ["'self'", 'fonts.com'],
+        imgSrc: ['img.com', 'data:'],
+        sandbox: ['allow-forms', 'allow-scripts'],
+        reportUri: '/report-violation',
+        objectSrc: ["'none'"],
+        upgradeInsecureRequests: true,
+        workerSrc: false  // This is not set.
     }
 }))
 
